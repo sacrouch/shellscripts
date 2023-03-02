3439629de33a778597d3cb01133ed4697bf65df2
root <root@localhost.localdomain>
2023-03-02 09:33:04 -0500

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -6,3 +6,6 @@ fi
 
 ## $1 is the first word on the command line
 ## after the command itself
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
